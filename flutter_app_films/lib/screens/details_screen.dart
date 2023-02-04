854236debf48c3543b17f7a760f041dd04a9d1c3
Claudia Cadec <claudiacadec@outlook.com>
2023-02-04 20:16:29 +0100

    commit

--- flutter_app_films/lib/screens/details_screen.dart
+++ flutter_app_films/lib/screens/details_screen.dart
@@ -9,10 +9,41 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: Center(
-        child: Text( "Details screen"),
-      ),
+
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? "no-movie";
+    
+    return   Scaffold(
+      
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppbar()
+
+        ],
+      )
+    );
+  }
+}
+class  _CustomAppbar extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverAppBar(
+backgroundColor: Colors.green,
+expandedHeight: 200,
+floating: false,
+pinned:true,
+flexibleSpace: FlexibleSpaceBar(
+
+centerTitle: true,
+title: Text("movie.title"),
+background: FadeInImage(
+ placeholder: AssetImage("assets/loading.gif"),
+image: NetworkImage ("https://via.placeholder.com/500x300"),
+fit:BoxFit.cover,
+),
+)
+
     );
   }
 }
